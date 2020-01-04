7a0177e3ebf0d92b1ed6f2f99f263f3809dc762e
Jungs <jungs6734@gmail.com>
2020-01-04 18:45:57 +0900

    BaseCharacter C++클래스 추가

---
+++ TwinStickShooter/Intermediate/Build/Win64/TwinStickShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/gowjr/gitProject/UnrealEngine/TwinStickShooter/Intermediate/Build/Win64/TwinStickShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ TwinStickShooter/Intermediate/Build/Win64/UE4Editor/Inc/TwinStickShooter/BaseCharacter.generated.h
@@ -0,0 +1,93 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TWINSTICKSHOOTER_BaseCharacter_generated_h
+#error "BaseCharacter.generated.h already included, missing '#pragma once' in BaseCharacter.h"
+#endif
+#define TWINSTICKSHOOTER_BaseCharacter_generated_h
+
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_SPARSE_DATA
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_RPC_WRAPPERS
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABaseCharacter(); \
+	friend struct Z_Construct_UClass_ABaseCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ABaseCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TwinStickShooter"), NO_API) \
+	DECLARE_SERIALIZER(ABaseCharacter)
+
+
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesABaseCharacter(); \
+	friend struct Z_Construct_UClass_ABaseCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ABaseCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TwinStickShooter"), NO_API) \
+	DECLARE_SERIALIZER(ABaseCharacter)
+
+
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABaseCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABaseCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABaseCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABaseCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABaseCharacter(ABaseCharacter&&); \
+	NO_API ABaseCharacter(const ABaseCharacter&); \
+public:
+
+
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABaseCharacter(ABaseCharacter&&); \
+	NO_API ABaseCharacter(const ABaseCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABaseCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABaseCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ABaseCharacter)
+
+
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_PRIVATE_PROPERTY_OFFSET
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_9_PROLOG
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_SPARSE_DATA \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_RPC_WRAPPERS \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_INCLASS \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_SPARSE_DATA \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TWINSTICKSHOOTER_API UClass* StaticClass<class ABaseCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TwinStickShooter_Source_TwinStickShooter_BaseCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
